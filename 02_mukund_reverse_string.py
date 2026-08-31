41fc61dfbff7a5eac516e605b3ca8b312dede893
mukundbrao-creator <mukund.b.rao@gmail.com>
2026-08-31 17:03:03 -0400

    Add files via upload

---
+++ 02_mukund_reverse_string.py
@@ -0,0 +1,6 @@
+string = input("Please enter your string: ")
+string2 = ''
+for i in string:
+    string2 = i + string2
+print("\nThe original string:", string)
+print("\nReversed String:", string2)
---
+++ 03_mukund_reverse_order.py
@@ -0,0 +1,4 @@
+n = int(input("Enter the value of n: "))
+print("From {0} to {1}:".format(n,1))
+for i in range (n, 0, -1):
+    print(i)
